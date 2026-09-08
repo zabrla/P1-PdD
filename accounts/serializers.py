c7d166cfc27d8f608f4c3782b3fec702199d5538
Carlos Morais <coelhocadu0909@gmail.com>
2026-09-08 08:21:54 -0300

    feat:criar serializers

---
+++ accounts/serializers.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
