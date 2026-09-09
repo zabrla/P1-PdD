52204abf05ee44dd1cad1518b56e492694898481
Rhennan Oliveira <reehsecundario@gmail.com>
2026-09-09 15:13:01 -0300

    feat: configura ASGI para signaling P2P

--- PdP/asgi.py
+++ PdP/asgi.py
@@ -1,16 +1,21 @@
-"""
-ASGI config for PdP project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PdP.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PdP.settings")
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+import signaling.routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(signaling.routing.websocket_urlpatterns)
+            )
+        ),
+    }
+)
